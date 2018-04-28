07d575370d159b98de72bd3b760e228b82a87ea0
David Hill <david@davidhill.ie>
2018-04-28 02:31:51 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,112 @@
+
+const { Observable, Subject, from } = require('rxjs');
+const { map, tap, filter, first, scan, switchMap} = require('rxjs/operators');
+
+
+
+
+
+
+
+// --- create fake events from an array ---
+// --- mash delay in between
+
+
+const timer$ = Observable.interval(1000);
+const eventsArray = [
+                      'disconnect', 
+                      'disconnect', 
+                      'disconnect', 
+                      'connect',
+                      'disconnect', 
+                      'disconnect', 
+                      'disconnect', 
+                      'disconnect',
+                      'disconnect'
+                    ];
+
+const events$ = Observable.from(eventsArray);
+
+const delayedEvents = Observable.zip(
+  timer$,
+  events$,
+  (_, connectChangeEvent) => connectChangeEvent
+);
+
+
+// end dummy event creation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const maxRetries = 4;
+
+const resetableCounter = function(predicate, countIncrease, countReset) {
+    return function(count, currentValue){
+      // count = predicate(currentValue) ? (count + 1) : 0;
+        if(predicate(currentValue)){ // do the test function
+          count = count + 1 // if true increase the counter
+          countIncrease(count); // run function
+        } else {
+          countReset(); // run function
+          count = 0; // if false reset the count to zero
+        }
+    return count;
+    }    
+};
+
+
+
+const caughtDisconnect = function(count){ 
+  console.log('disconnect caught: ' + count)  
+}
+
+const countReset = function(){ 
+  console.log('counter Reset to zero')  
+}
+
+
+const example = delayedEvents.pipe(
+  //tap(val => console.log(`Value ${val}`)),
+	scan(
+    resetableCounter(eventName => (eventName === 'disconnect'), caughtDisconnect, countReset)
+    ,0 // start count
+  ),
+  filter(count => count === maxRetries), // ignore counts below max
+  map( () => 'Max retries reached'), 
+  first() // finish on first event
+)
+
+
+  example.subscribe(function(result) {
+    // disconnect, give up! react as you please   
+    console.log('finalresult: ' + result);
+  }); 
+
+/*
+ const subscribe = example.subscribe({
+    next(result) {
+      // disconnect, give up! react as you please   
+      console.log('result: ' + result);
+    },
+    error(err) { 
+      console.log(err);
+    },
+    complete() {
+      console.log('Done!');
+    }
+ });
+
+*/
+
+//dd
